8f53a4bd68cb2142d9301353eff3705f0cb06f41
wakeme0129 <95006265+wakeme0129@users.noreply.github.com>
2022-02-02 09:56:21 +0900

    Create digitalClick.js

---
+++ digitalClick.js
@@ -0,0 +1,21 @@
+function start(){
+   
+   window.setInterval(clock,1000);
+}
+
+function clock(){
+  
+  let now=new Date();
+  let hh=now.getHours();
+  let mm=now.getMinutes();
+  let ss=now.getSeconds();
+  
+  hh=hh<10?  "0"+hh:hh;
+  mm=mm<10?  "0"+mm:mm;
+  ss=ss<10?  "0"+ss:ss;
+  
+  
+  let result=document.getElementById("result");
+  
+  result.texyContent=hh +":"+mm+":"+ss;
+}
